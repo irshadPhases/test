566725a77345feea4f5b3286376366513b65d17f
Irshad muhammed <irshad.mv@phases.io>
2023-01-20 11:06:11 +0530

    initial commit

---
+++ resources/js/Welcome.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <h1>Landing page {{dd}}</h1>
+        <Home/>
+    </div>
+</template>
+<script>
+import Home from './components/Home.vue';
+export default {
+    components:{
+        Home,
+    },
+    return:{
+        pl:""
+    }
+        
+}
+</script>
